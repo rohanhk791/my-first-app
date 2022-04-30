4db3f03084d805ad65319d4d1853fedb81ebd673
Rohan H K <rohanhk2@gmail.com>
2022-04-30 11:26:36 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
+import photo1 from './assets';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={photo1.jpg} className="App-logo" alt="logo" />
+        <h1>
+          React nodejs
+        </h1>
+        <p>React is a declarative, efficient, and flexible JavaScript library for building user interfaces. It lets you compose complex UIs from small and isolated pieces of code called “components”.
         </p>
         <a
           className="App-link"
